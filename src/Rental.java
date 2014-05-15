9e6566fb31802592db54146ac0934fe202870881
Andreas Beckmann <beckmann.herxheim@web.de>
2014-05-15 14:58:52 +0200

    Used Extract Method and Moved Method for <getFrequentRenterPoints>, extracted out of <statement>-Method

--- src/Rental.java
+++ src/Rental.java
@@ -14,7 +14,7 @@ class Rental {
     
 	double getCharge() {
 	    double result = 0;
-		//determine amounts for each line
+	    
 		switch (getMovie().getPriceCode()) {
 		    case Movie.REGULAR:
 		    	result += 2;
@@ -31,5 +31,11 @@ class Rental {
 		    	break;
 		}
 		return result;
+	}
+	
+	int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
+		    return 2;
+		else return 1;
 	}
 }
