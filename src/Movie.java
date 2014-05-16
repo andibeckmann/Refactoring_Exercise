5f8881a7e5ec97ab2dee76bc7dc62c2f39499468
Andreas Beckmann <beckmann.herxheim@web.de>
2014-05-16 15:18:59 +0200

    Created new class Price and subclasses, used Move Method and Replace Conditional with Polymorphism für <getCharge> und <getFrequentRenterPoints>-Methoden

--- src/Movie.java
+++ src/Movie.java
@@ -3,7 +3,7 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private Price price;
+    Price price;
     
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
@@ -32,29 +32,10 @@ public class Movie {
     }
     
     double getCharge(int daysRented) {
-	    double result = 0;
-	    
-		switch (getPriceCode()) {
-		    case Movie.REGULAR:
-		    	result += 2;
-		    	if (daysRented > 2)
-		        	result += (daysRented - 2) * 1.5;
-		    	break;
-		    case Movie.NEW_RELEASE:
-		    	result += daysRented * 3;
-		    	break;
-		    case Movie.CHILDRENS:
-		    	result += 1.5;
-		    	if (daysRented > 3)
-		        	result += (daysRented - 3) * 1.5;
-		    	break;
-		}
-		return result;
+	    return price.getCharge(daysRented);
 	}
     
-	int getFrequentRenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) 
-		    return 2;
-		else return 1;
-	}
+    int getFrequentRenterPoints(int daysRented) {
+    	return price.getFrequentRenterPoints(daysRented);
+    }
 }
